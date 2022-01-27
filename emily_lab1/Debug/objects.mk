d89541fdea9de4ca75de7792320f0271e37148c6
emilyyeh123 <91395263+emilyyeh123@users.noreply.github.com>
2022-01-27 00:21:57 -0800

    Delete emily_lab1/Debug directory

--- emily_lab1/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -llibc.a -l"C:/Users/emily/bishop_ecpe155_2022/TivaWare/driverlib/ccs/Debug/driverlib.lib"
-
