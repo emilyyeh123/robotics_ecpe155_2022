2cdeac66219c20e6cfe450dbdccff060b35b4093
emilyyeh123 <e_yeh1@u.pacific.edu>
2022-02-20 11:57:49 -0800

    Creating Lab_3 main.c

---
+++ Lab_3/main.c
@@ -0,0 +1,28 @@
+
+
+/**
+ * Lab 3 main.c
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "driverlib/debug.h"
+#include "driverlib/pwm.h"
+#include "driverlib/pin_map.h"
+#include "inc/hw_gpio.h"
+#include "driverlib/rom.h"
+#include "inc/tm4c123gh6pm.h"
+
+#include "PWM.h"
+#include "Motor_Control.h"
+
+int main(void)
+{
+	return 0;
+}
