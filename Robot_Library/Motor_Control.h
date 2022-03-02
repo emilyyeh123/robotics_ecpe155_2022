362373f5bb1898e00be1fd67ab6d08add39e3c62
emilyyeh123 <e_yeh1@u.pacific.edu>
2022-03-01 16:08:18 -0800

    removed ICR attempt in motor control. Will complete that in the future, but for now will rely on circumference to get position.

--- Robot_Library/Motor_Control.h
+++ Robot_Library/Motor_Control.h
@@ -9,18 +9,10 @@
 #define MOTOR_CONTROL_H_
 
 
-struct ICR{
-    uint8_t x;
-    uint8_t y;
-    uint8_t theta;
-};
-
 void initMotor(uint16_t period);
 void motorForward(uint16_t PWLeft, uint16_t PWRight);
 void motorBackward(uint16_t PWLeft, uint16_t PWRight);
 void motorStop();
-
-//void ICRPosition(){}
 
 
 #endif /* MOTOR_CONTROL_H_ */
