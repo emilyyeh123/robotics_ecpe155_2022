153d067e9f8ead279c9f0bbd5779ed94d19eb558
emilyyeh123 <e_yeh1@u.pacific.edu>
2022-03-19 23:54:29 -0700

    uncached debugs

--- Lab_5/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"C:/Users/emily/bishop_ecpe155_2022/Robot_Library/Debug/Robot_Library.lib" -l"C:/Users/emily/bishop_ecpe155_2022/TivaWare/driverlib/ccs/Debug/driverlib.lib" -llibc.a
-
