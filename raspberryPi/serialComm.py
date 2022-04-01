e8da9a4f7af3d39dc47abbc9410b7bbcfa0d6da5
Emily <e_yeh1@u.pacific.edu>
2022-04-01 20:04:27 +0100

    sending & recieving data

--- raspberryPi/serialComm.py
+++ raspberryPi/serialComm.py
@@ -4,13 +4,13 @@ import serial
 import time
 
 def main():
+    ser = serial.Serial("/dev/ttyS0") # open named port
     for i in range(5):
-        ser = serial.Serial("/dev/ttyS0") # open named port
         ser.write(bytes(b'Hello'))
         time.ctime(30)
     #ser.baudrate = 9600
-    #data = ser.read(1) #read 10 characters from serial port
-    #ser.write(data)
+    data = ser.read(10) #read 10 characters from serial port
+    ser.write(data)
     ser.close()
 
 if __name__ == "__main__":
