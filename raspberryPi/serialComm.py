a31c7ccf28b36931c686fce0a40070605409fe59
Emily <e_yeh1@u.pacific.edu>
2022-04-07 01:58:23 +0100

    serialComm.py works!

--- raspberryPi/serialComm.py
+++ raspberryPi/serialComm.py
@@ -5,30 +5,32 @@ import time
 import struct
 
 def main():
+    ser = serial.Serial("/dev/ttyS0") # open named port
+
     startCommand = b'\xaa'
     endCommand = b'\x55'
     motorForward = b'\x81' 
 
-    ser = serial.Serial("/dev/ttyS0") # open named port
+    # send start command
+    packData = struct.pack('2c', startCommand, motorForward)
+    print("transmit ", packData)
+    trans = ser.write(packData)
+    rec = ser.read(1)
+    print("recieve ", rec)
+    unpackData = struct.unpack('c', rec)
+    print("unpack ", unpackData)
+    
+    if(unpackData == b'\x55'):
+        print("Data unpacked properly!")
 
-    # when sending data, need to pack to convert to C struct
-    packData = struct.pack('3c', startCommand, motorForward, endCommand)
-    ser.write(packData)
+        #for i in range(2):
+            #packData = b'' 
+            #print("recieving index:", i)
+            #recieve = ser.read(1)
+            #packData += struct.pack('c', recieve)
+        #print("transmitting", packData)
 
-    '''
-    # when recieving data, need to unpack to convert to python struct
-    data = ser.read(10) #read 10 characters from serial port
-    unpackData = struct.unpack, ('c', data)
-    ser.write(data)
     ser.close()
-    '''
-
-    #packData = struct.pack('c', b'o')
-    #print("Packed Data: ", packData, "\n")
-    #print("Unpacked Data: ", unpackData, "\n")
-
-    #binToHex = hex(int('10000001', 2))
-    #print(binToHex)
 
 if __name__ == "__main__":
     main()
