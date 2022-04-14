9da59e3589aff6f222470efe3219799a24c15a13
emilyyeh123 <e_yeh1@u.pacific.edu>
2022-04-14 15:39:43 -0700

    tiva properly receiving and storing data

--- Lab_6/main.c
+++ Lab_6/main.c
@@ -28,8 +28,22 @@
 #include "bumpSensor.h"
 #include "Trans_Reciever.h"
 
-char packet_send[3] = {0xAA, 0xFF, 0x55};
-char packet_rec[2];
+// packet structure
+#define startCommand 0xAA
+#define endCommand 0x55
+
+// motor commands
+#define motorForward 0x81
+#define motorBackward 0x82
+#define right45 0x83
+#define left45 0x84
+#define right90 0x85
+#define left90 0x86
+
+char packet_send[3] = {startCommand, 0x02, endCommand};
+// can receive up to 8 bytes of data
+// 3 bytes reserved for start (0xAA), command, end (0x55)
+char packet_rec[8];
 
 int main(void)
 {
@@ -37,28 +51,41 @@ int main(void)
     initLED();
 
     while(1){
+        //clear packet_rec
+        for(int i = 0; i < 8; i++){
+            packet_rec[i] = 0x00;
+        }
 
         // Indicate ready to receive
         displayGreenLED();
 
-        // Wait until a response is received
-        while(!UARTCharsAvail(UART1_BASE)) {}
+        // Wait until a byte is received
+        while(!UARTCharsAvail(UART1_BASE)){}
 
-        // Store the incoming data to a specified array
-        for(int i = 0; i < sizeof(packet_rec); i++) {
-                packet_rec[i] = UARTCharGet(UART1_BASE);
+        // run until all bytes of data have been received
+        // or until end command has been received
+        int count = 0;
+        while(1){
+            // Store the incoming data
+            packet_rec[count] = UARTCharGet(UART1_BASE);
+            if(packet_rec[count] == endCommand){break;}
+            count++;
         }
 
         clearLED();
 
-        // If the initialize flag is receive
-        if (packet_rec[0] == 0xAA) {
+        // check for start command
+        if(packet_rec[0] == startCommand){
+            for(int i = 0; i < 3; i++) {
+                 UARTCharPut(UART1_BASE,packet_send[i]);
+             }
+        }
 
+        // If the initialize flag is received
+        /*if (packet_rec[0] == startCommand) {
             switch(packet_rec[1]) {
-
             // When THIS command is received
             case 0x81:
-                displayRedLED();
                 for(int i = 0; i < 3; i++) {
                     UARTCharPut(UART1_BASE,packet_send[i]);
                     clearLED();
@@ -72,7 +99,6 @@ int main(void)
 
         displayBlueLED();
         SysCtlDelay(5000000);
-        clearLED();
-
-        }
+        clearLED();*/
     }
+}
