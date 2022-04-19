81b775b53e94ba1bdcfd8ba952a8a59aaa1e57db
emilyyeh123 <e_yeh1@u.pacific.edu>
2022-04-19 01:33:16 -0700

    moved all of lab_6 main into a function in trans_receiver. created lab7 main in preparation for obstacle avoidance. updated packet protocol with command for navigating to xy coordinates.

--- Robot_Library/Trans_Reciever.h
+++ Robot_Library/Trans_Reciever.h
@@ -8,11 +8,29 @@
 #ifndef TRANS_RECIEVER_H_
 #define TRANS_RECIEVER_H_
 
-void initSerial();
-void recieveMess(char *packet_rec, char *packet_send);
-void jobComplete();
-void objectDetected(char *packet_send);
+// packet structure
+#define startCommand 0xAA
+#define endCommand 0x55
 
+// Movement Commands
+#define moveForward 0x01
+#define moveBackward 0x02
+#define turnRight 0x03
+#define turnLeft 0x04
+#define moveToXY 0x05
+
+// IR Commands
+#define rightIR 0x11
+#define leftIR 0x12
+#define backIR 0x13
+
+void initSerial();
+//void jobComplete(char *packet_send);
+//void objectDetected(char *packet_send);
+void performAction(char *packet_rec, char *packet_Send);
+void initRecPacket(char *packet_rec);
+void initSendPacket(char *packet_send);
+void storeReceivedPacket(char *packet_rec);
 
 
 #endif /* TRANS_RECIEVER_H_ */
