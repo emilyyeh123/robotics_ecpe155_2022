be45157f62b5b1a7fce9e85f8aac34d73a86a346
Emily <e_yeh1@u.pacific.edu>
2022-04-19 03:53:24 +0100

    updating pi stuff

--- raspberryPi/uartComm.py
+++ raspberryPi/uartComm.py
@@ -15,7 +15,9 @@ turnRight = 0x03
 turnLeft = 0x04
 
 # IR Commands
-# INCLUDE IR COMMANDS HERE
+rightIR = 0x11
+leftIR = 0x12
+backIR = 0x13
 
 # initialize serial
 ser = serial.Serial("/dev/ttyS0", 9600) 
@@ -39,8 +41,8 @@ def moveMenu():
 		print("\t0) cancel")
 		print("\t1) move forward")
 		print("\t2) move backward")
-		print("\t3) turn right")
-		print("\t4) turn left")
+		print("\t3) turn right 90 degrees")
+		print("\t4) turn left 90 degrees")
 
 		# receive user input
 		directionInp = int(input("\t>>> "))
@@ -54,29 +56,36 @@ def moveMenu():
 			return
 
 def sendMovePacket(dir):
-	print("\n\tHow far would you like to move?")
-	print("\tEnter a number between 1 and 255 (movement is tick-based)")
-	dist = int(input("\t>>> "))
-	while dist < 1 or dist > 255:
-		# receive user input
-		print("\tINVALID TRY AGAIN")
-		dist = int(input("\t>>> "))
-
 	packet = []
 	packet.append(struct.pack('B', startCommand))
 
 	if dir == 1:
+		print("\n\tHow far would you like to move?")
+		print("\tEnter a number between 1 and 255 (movement is tick-based)")
+		dist = int(input("\t>>> "))
+		while dist < 1 or dist > 255:
+			# receive user input
+			print("\tINVALID TRY AGAIN")
+			dist = int(input("\t>>> "))
+
 		packet.append(struct.pack('B', moveForward))
 		packet.append(struct.pack('B', dist))
 	elif dir == 2:
+		print("\n\tHow far would you like to move?")
+		print("\tEnter a number between 1 and 255 (movement is tick-based)")
+		dist = int(input("\t>>> "))
+		while dist < 1 or dist > 255:
+			# receive user input
+			print("\tINVALID TRY AGAIN")
+			dist = int(input("\t>>> "))
+
 		packet.append(struct.pack('B', moveBackward))
 		packet.append(struct.pack('B', dist))
-	elif dir == 3:
+	if dir == 3:
 		packet.append(struct.pack('B', turnRight))
-		packet.append(struct.pack('B', dist))
 	elif dir == 4:
 		packet.append(struct.pack('B', turnLeft))
-		packet.append(struct.pack('B', dist))
+
 
 	packet.append(struct.pack('B', endCommand))
 	# send all requests
@@ -87,22 +96,74 @@ def sendMovePacket(dir):
 	time.sleep(0.5)
 
 	respPacket = []
-	for i in range(3):
-		resp = ser.read()
-		resp = struct.unpack('B', resp)
-		resp = hex(resp[0])
-		print("\tFirst byte of data: ", resp)
+	resp = ser.read()
+	resp = struct.unpack('B', resp)
+	resp = resp[0]
+	print("\tFirst byte of data (converted to hex): ", hex(resp))
 
-		if resp == startCommand:
-			while 1:
-				resp = ser.read()
-				resp = struct.unpack('B', resp)
-				resp = hex(resp[0])
-				print("\tNext byte of data: ", resp)
-				if resp != endCommand:
-					respPacket.append(resp)
-				else:
-					break
+	if resp == startCommand:
+		while 1:
+			resp = ser.read()
+			resp = struct.unpack('B', resp)
+			resp = resp[0]
+			print("\tNext byte of data (converted to hex): ", hex(resp))
+			if resp != endCommand:
+				respPacket.append(resp)
+			else:
+				break
+
+def sendIRCommand():
+	while 1:
+		print("Which IR sensor would you like to retreive data from?")
+		print("\t0) cancel")
+		print("\t1) Right IR Sensor")
+		print("\t2) Left IR Sensor")
+		print("\t3) Back IR Sensor")
+
+		# receive user input
+		IRinp = int(input("\t>>> "))
+		if IRinp == 0:
+			return
+		elif IRinp < 1 or IRinp > 3:
+			# default
+			print("\tNOT A VALID OPTION\n")
+		else:
+			break
+
+	packet = []
+	packet.append(struct.pack('B', startCommand))
+
+	if IRinp == 1:
+		packet.append(struct.pack('B', rightIR))
+	elif IRinp == 2:
+		packet.append(struct.pack('B', leftIR))
+	elif IRinp == 3:
+		packet.append(struct.pack('B', backIR))
+
+	packet.append(struct.pack('B', endCommand))
+	# send all requests
+	for i in range(len(packet)):
+		ser.write(packet[i])
+		print("\ttransmitting: ", packet[i])
+
+	time.sleep(0.5)
+
+	respPacket = []
+	resp = ser.read()
+	resp = struct.unpack('B', resp)
+	resp = resp[0]
+	print("\tFirst byte of data (converted to hex): ", hex(resp))
+
+	if resp == startCommand:
+		while 1:
+			resp = ser.read()
+			resp = struct.unpack('B', resp)
+			resp = resp[0]
+			print("\tNext byte of data (converted to hex): ", hex(resp))
+			if resp != endCommand:
+				respPacket.append(resp)
+			else:
+				break
 
 def main():
 	while 1:
@@ -115,7 +176,7 @@ def main():
 			moveMenu()
 		elif menuInp == 2:
 			# call IR function
-			pass
+			sendIRCommand()
 		else:
 			# default
 			print("NOT A VALID OPTION")
