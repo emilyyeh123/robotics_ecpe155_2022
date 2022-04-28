0d898a69bec6bf56421d58ce074683b74957efc9
Emily <e_yeh1@u.pacific.edu>
2022-04-28 08:03:34 +0100

    testing hsv value at specific point in picture

--- raspberryPi/piCam/colorDetect.py
+++ raspberryPi/piCam/colorDetect.py
@@ -4,7 +4,10 @@ import picamera
 import time
 import cv2
 import numpy as np
+import PIL
+from PIL import Image
 #import matplotlib.pyplot as plt
+
 
 def main():
 	# setup camera - orient right-side up and shrink resolution
@@ -19,11 +22,24 @@ def main():
 		#camera.stop_preview()
 		#camera.capture('/home/pi/bishop_ecpe155_2022/raspberryPi/piCam/piImages/img%s.jpg' % i)
 
-	# open camera in window, stay open for a few seconds before closing again
-	# window = (screen coord x, y, resolution width, height)
-	#camera.start_preview(fullscreen = False, window = (100, 200, 1280, 720))
-	#time.sleep(13)
-	#camera.stop_preview()
+	img = cv2.imread('/home/pi/bishop_ecpe155_2022/raspberryPi/piCam/piImages/img%s.jpg' % 4)
+	#img = cv2.imread('/home/pi/Desktop/colorDetection.png')
+	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+	hsvColor = (hsv[640, 360])
+	print(hsvColor)
+	#lowerRed = np.array([60,60,100])
+	#upperRed = np.array([100,50,100])
+	#mask = cv2.inRange(hsv, lowerRed, upperRed)
+	
+	#img = Image.open('/home/pi/bishop_ecpe155_2022/raspberryPi/piCam/piImages/img%s.jpg' % 4)
+	#colors = img.getpixel((640,360))
+	#print(colors)
+	#cv2.imshow("hsv", hsv)
+	#cv2.imshow("mask", mask)
+	#img = cv2.imread('/home/pi/bishop_ecpe155_2022/raspberryPi/piCam/piImages/img%s.jpg' % 4)
+	#hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+	#cv2.imshow("mask2", hsv)
+	#cv2.waitKey(0)
 
 	'''
 	# take 5 pictures and do color detection
@@ -32,18 +48,19 @@ def main():
 		img = cv2.imread('/home/pi/bishop_ecpe155_2022/raspberryPi/piCam/piImages/img%s.jpg' % i)
 
 		# hue, saturation, brightness
-		hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+		#hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
 		# color: red range
 		lowerRange = np.array([0,100,70])
 		upperRange = np.array([0,100,100])
-		mask = cv2.inRange(hsv, lowerRange, upperRange)
+		mask = cv2.inRange(img, lowerRange, upperRange)
 		#lowerMask = cv2.inRange(img, np.array([0,100,20]), np.array([10,255,255]))
 		#upperMask = cv2.inRange(img, np.array([160,100,20]), np.array([179,255,255]))
 		#fullMask = lowerMask + upperMask
 
 		#result = cv2.bitwise_and(img.copy(), img.copy(), mask = fullMask)
 
+		cv2.imshow("img", img)
 		cv2.imshow("mask", mask)
 		#cv2.imshow("result", result)
 
